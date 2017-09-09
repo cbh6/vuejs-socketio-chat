b7561ac60906454c15872d05092c3fcb0cf8a6ac
cbh6 <cri.bh6@gmail.com>
2017-09-09 15:09:14 +0200

    msg handling

--- app/app.js
+++ app/app.js
@@ -1,26 +1,40 @@
-var socket = io();
+(function(){
 
-var app = new Vue({
-    el: '#chat',
-    data: {
-        messages: [],
-        message: ''
-    },
-    methods: {
-        onSubmit: function(event) {
-            event.preventDefault();
+    var socket = io();
+
+    var app = new Vue({
+        el: '#chat',
+        data: {
+            messages: [],
+            message: ''
         },
-        send: function(event) {
-            socket.emit('send-msg', this.message);
-            this.message = '';
+        methods: {
+            // onSubmit: function(event) {
+            //     event.preventDefault();
+            // },
+            send: function() {
+                if(this.message){
+                    socket.emit('send-msg', this.message);
+                    this.message = '';
+                }
+            },
+            scrollToEnd: function() {       
+                var container = this.$el.querySelector("#messages");
+                container.scrollTop = container.scrollHeight;
+            },
+        },
+        updated(){
+            this.scrollToEnd();
         }
-    }
-});
+    });
 
-socket.on('read-msg', function(data){
-    app.messages.push(data);
-});
+    socket.on('read-msg', function(message){
+        app.messages.push({text : message.text, user : message.user, date : message.date});
+    });
 
-socket.on('user-connected', function(userId) {
-    app.messages.push('User ' + userId + ' has been connected');
-});
+    socket.on('user-connected', function(userId) {
+       // app.messages.push('User ' + userId + ' has been connected');
+       console.log('User ' + userId + ' has been connected');
+    });
+
+})();
--- server.js
+++ server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var dateFormat = require('dateformat');
 
 app.set('port', (process.env.PORT || 3000));
 app.use('/npm', express.static('node_modules'));
+app.use(express.static('app'));
 
 app.get('/', function(request, response) {
     response.sendFile(__dirname + '/app/index.html');
@@ -22,10 +24,11 @@ io.on('connection', function(socket) {
 	socket.broadcast.emit('user-connected', socket.id);
 
     socket.on('send-msg', function(message) {
-        io.emit('read-msg', message);
+        io.emit('read-msg', { text : message, user : socket.id, date : dateFormat(new Date(), 'shortTime') });
     });
 
     socket.on('disconnect', function() {
-        io.emit('read-msg', 'User has disconnected.');
+        //io.emit('read-msg', 'User has disconnected.');
+        console.log('User ' + socket.id + ' has disconnected');
     });
 });
